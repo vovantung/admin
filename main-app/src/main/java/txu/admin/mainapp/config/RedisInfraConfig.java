b45fcba1c21f21b5e015eab99d35791a5583a6dc
vovantung <vovantungdt123@gmail.com>
2025-12-22 23:46:39 +0700

    cm102

--- main-app/src/main/java/txu/admin/mainapp/config/RedisInfraConfig.java
+++ main-app/src/main/java/txu/admin/mainapp/config/RedisInfraConfig.java
@@ -4,16 +4,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisInfraConfig {
+
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(
+//            RedisConnectionFactory factory
+//    ) {
+//        RedisTemplate<String, Object> tpl = new RedisTemplate<>();
+//        tpl.setConnectionFactory(factory);
+//        return tpl;
+//    }
+
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(
             RedisConnectionFactory factory
     ) {
-        RedisTemplate<String, Object> tpl = new RedisTemplate<>();
-        tpl.setConnectionFactory(factory);
-        return tpl;
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        // 🔥 BẮT BUỘC
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        template.afterPropertiesSet();
+        return template;
     }
 }
